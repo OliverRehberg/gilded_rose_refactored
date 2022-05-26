b933ce7d374da570d02b8d1260bd586f3fefcc88
oliver.or.rehberg <oliver.or.rehberg@fau.de>
2022-05-26 07:28:26 +0200

    Introduce getter and setter methods
    This change adds setter and getter methods for the attributes of the
    item class and switches to calls of them in the other methods.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -19,34 +19,49 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+    def get_name(self):
+        return self.name
+
+    def get_quality(self):
+        return self.quality
+
+    def set_quality(self, new_quality):
+        self.quality = new_quality
+
+    def get_sell_in(self):
+        return self.sell_in
+
+    def set_sell_in(self, new_sell_in):
+        self.sell_in = new_sell_in
+
     def update_quality(self):
-        if self.name != "Aged Brie" and self.name != "Backstage passes to a TAFKAL80ETC concert":
+        if self.get_name() != "Aged Brie" and self.get_name() != "Backstage passes to a TAFKAL80ETC concert":
             self.decrease_quality_if_feasible()
         else:
             self.increase_quality_if_feasible()
-            if self.name == "Backstage passes to a TAFKAL80ETC concert":
-                if self.sell_in < 11:
+            if self.get_name() == "Backstage passes to a TAFKAL80ETC concert":
+                if self.get_sell_in() < 11:
                     self.increase_quality_if_feasible()
-                if self.sell_in < 6:
+                if self.get_sell_in() < 6:
                     self.increase_quality_if_feasible()
-        if self.sell_in == 0:
-            if self.name != "Aged Brie":
-                if self.name != "Backstage passes to a TAFKAL80ETC concert":
+        if self.get_sell_in() == 0:
+            if self.get_name() != "Aged Brie":
+                if self.get_name() != "Backstage passes to a TAFKAL80ETC concert":
                     self.decrease_quality_if_feasible()
                 else:
-                    self.quality = 0
+                    self.set_quality(0)
             else:
                 self.increase_quality_if_feasible()
 
     def increase_quality_if_feasible(self):
-        if self.quality < 50:
-            self.quality = self.quality + 1
+        if self.get_quality() < 50:
+            self.set_quality(self.quality + 1)
 
     def decrease_quality_if_feasible(self):
-        if self.quality > 0:
-            if self.name != "Sulfuras, Hand of Ragnaros":
-                    self.quality = self.quality - 1
+        if self.get_quality() > 0:
+            if self.get_name() != "Sulfuras, Hand of Ragnaros":
+                    self.set_quality(self.quality - 1)
 
     def update_sell_in(self):
-        if self.name != "Sulfuras, Hand of Ragnaros":
-            self.sell_in = self.sell_in - 1
+        if self.get_name() != "Sulfuras, Hand of Ragnaros":
+            self.set_sell_in(self.sell_in - 1)
